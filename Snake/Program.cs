3fc0eb2ec052241b451e7160797a24c1f96a914b
KaluginDV <interrupted-man@yandex.ru>
2017-07-30 21:28:06 +0800

    Добавили вертикальные и горизонтальные линии

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,21 +10,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(6, 8, '&');
-            Point p4 = new Point(10, 10, 'H');
+            HorisontalLine hLine = new HorisontalLine(3, 105, 3, '-');
+            hLine.Draw();
 
-            List<Point> numList = new List<Point>();
-            numList.Add(p1);
-            numList.Add(p2);
-            numList.Add(p3);
-            numList.Add(p4);
-
-            foreach (Point point in numList)
-            {
-                point.Draw();
-            }
+            VerticalLine vLine = new VerticalLine(4, 24, 3, '|');
+            vLine.Draw();
 
             Console.ReadLine();
         }
