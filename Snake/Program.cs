0c4509e19bbae774d8df61422e670418e464c66e
KaluginDV <interrupted-man@yandex.ru>
2017-07-30 19:04:18 +0800

    Создан список объектов класс Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,11 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(4, 5, '#');
+            Point p3 = new Point(6, 8, '&');
+            Point p4 = new Point(10, 10, 'H');
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            List<Point> numList = new List<Point>();
+            numList.Add(p1);
+            numList.Add(p2);
+            numList.Add(p3);
+            numList.Add(p4);
+
+            foreach (Point point in numList)
+            {
+                point.Draw();
+            }
 
             Console.ReadLine();
         }
