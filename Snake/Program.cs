f3f1619d6385eef8e3b938b4701753485a53e45e
KaluginDV <interrupted-man@yandex.ru>
2017-07-31 23:08:46 +0800

    добавили класс стены и столкновения с ними

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,17 +16,10 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
 
-            //Отрисовка рамки
-            HorisontalLine hLineUp = new HorisontalLine(1, 77, 0, '-');
-            hLineUp.Draw();
-            HorisontalLine hLineDown = new HorisontalLine(1, 77, 24, '-');
-            hLineDown.Draw();
-            VerticalLine vLineLeft = new VerticalLine(0, 24, 0, '|');
-            vLineLeft.Draw();
-            VerticalLine vLineRight = new VerticalLine(0, 24, 78, '|');
-            vLineRight.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-            //Отрисовка змейки
+            // Отрисовка точек			
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
@@ -34,22 +27,52 @@ namespace Snake
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
-            
+
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
                 }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Автор: Дмитрий Калугин", xOffset + 2, yOffset++);
+            WriteText("Специально для Сонечки", xOffset + 1, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
